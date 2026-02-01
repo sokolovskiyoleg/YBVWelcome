2889b793772fc2ac40edaf4686a64523a9b9f264
oleg <osn298@gmail.com>
2026-02-01 16:19:54 +0300

    Fix colors

--- src/main/java/org/yabogvk/ybvwelcome/utils/MessageUtils.java
+++ src/main/java/org/yabogvk/ybvwelcome/utils/MessageUtils.java
@@ -1,38 +1,41 @@
 package org.yabogvk.ybvwelcome.utils;
 
-import org.bukkit.command.CommandSender;
-import org.yabogvk.ybvwelcome.color.ColorizerProvider;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-
-import java.util.regex.Pattern;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.yabogvk.ybvwelcome.color.ColorizerProvider;
 
 public class MessageUtils {
 
-    private static final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)" + String.valueOf('&') + "[0-9A-FK-ORX]");
     private static final LegacyComponentSerializer LEGACY_SERIALIZER = LegacyComponentSerializer.builder().hexColors().build();
 
     public static Component parse(String message) {
-        if (message == null || message.isEmpty()) return Component.empty();
+        if (message == null || message.isEmpty()) {
+            return Component.empty();
+        }
         String colored = colorize(message);
-
         return LEGACY_SERIALIZER.deserialize(colored);
     }
 
-
     public static String colorize(String message) {
-        if (message == null || message.isEmpty()) return "";
+        if (message == null || message.isEmpty()) {
+            return "";
+        }
         return ColorizerProvider.COLORIZER.colorize(message);
     }
 
     public static String stripColors(String message) {
-        if (message == null || message.isEmpty()) return "";
-        return STRIP_COLOR_PATTERN.matcher(message).replaceAll("");
+        if (message == null || message.isEmpty()) {
+            return "";
+        }
+        String colored = colorize(message);
+        return ChatColor.stripColor(colored);
     }
 
     public static void sendMessage(CommandSender sender, String message) {
         if (sender != null && message != null) {
-            sender.sendMessage(colorize(message));
+            sender.sendMessage(parse(message));
         }
     }
 }
